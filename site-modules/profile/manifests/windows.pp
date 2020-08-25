2f56b3c1d0f711fc7d5a432c2707ea01365f70af
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-08-25 14:26:06 +1000

    create directory; change to use variables

--- site-modules/profile/manifests/windows.pp
+++ site-modules/profile/manifests/windows.pp
@@ -1,16 +1,24 @@
 # profile for windows
 class profile::windows {
-  user { 'gluser':
+  $user  = 'gluser'
+  $group = 'glgroup'
+
+  user { $user:
     ensure     => present,
     forcelocal => true,
   }
-  group { 'glgroup':
+  group { $group:
     ensure  => present,
-    members => ['gluser'],
+    members => [$user],
   }
-  dsc_userrightsassignment { 'gluser_rights':
+  dsc_userrightsassignment { "${user}_Log_on_as_a_service":
     dsc_ensure   => present,
-    dsc_identity => 'gluser',
+    dsc_identity => $user,
     dsc_policy   => 'Log_on_as_a_service',
+  }
+  file { "C:\\ProgramData\\${user}":
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
   }
 }

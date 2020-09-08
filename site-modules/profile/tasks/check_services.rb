2daf3153dca906b1ab0e1fbede2020f294bb85aa
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-08 16:28:46 +1000

    set path to puppet_cmd

--- site-modules/profile/tasks/check_services.rb
+++ site-modules/profile/tasks/check_services.rb
@@ -2,6 +2,13 @@
 require 'json'
 require 'yaml'
 
-service_output = %x[puppet resource service --to_yaml]
+# Set path to puppet_cmd based on whether windows or not
+if (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
+  puppet_cmd = "#{ENV['programfiles']}/Puppet Labs/Puppet/bin/puppet"
+else
+  puppet_cmd = '/opt/puppetlabs/puppet/bin/puppet'
+end
+
+service_output = %x[#{puppet_cmd} resource service --to_yaml]
 
 puts JSON.pretty_generate(YAML.load(service_output))

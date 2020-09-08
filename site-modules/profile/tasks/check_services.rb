ded108ede01099904dbc54e7a2f8867633d74bd4
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-08 17:04:46 +1000

    let windows work out the path itself

--- site-modules/profile/tasks/check_services.rb
+++ site-modules/profile/tasks/check_services.rb
@@ -2,9 +2,11 @@
 require 'json'
 require 'yaml'
 
-# Set path to puppet_cmd based on whether windows or not
+# most platforms have puppet in path; however rhel6 seems to need help finding it on occasion
+# for now let windows work out the path itself rather than messing/mixing slashes
 if (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
-  puppet_cmd = "#{ENV['programfiles']}/Puppet Labs/Puppet/bin/puppet"
+  puppet_cmd = 'puppet'
+  #puppet_cmd = "#{ENV['programfiles']}/Puppet Labs/Puppet/bin/puppet"
 else
   puppet_cmd = '/opt/puppetlabs/puppet/bin/puppet'
 end

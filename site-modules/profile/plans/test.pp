bbf673638de735b585076031e441f41d5f556a67
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-11 12:30:57 +1000

    typo

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -1,7 +1,7 @@
 plan profile::test (
   TargetSpec $targets,
 ) {
-  run_task(facter_tasks, $targets, '_catch_errors' => true)
-  run_task(facter_tasks, $targets)
+  run_task(facter_task, $targets, '_catch_errors' => true)
+  run_task(facter_task, $targets)
   run_plan(facts, targets => $targets)
 }

0bca80dc37f30f93fa0ed8d41f5ca626ad35e412
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-11 18:08:11 +1000

    specify tidypath for testing

--- site-modules/profile/plans/windows_tidy_c_tmpfiles.pp
+++ site-modules/profile/plans/windows_tidy_c_tmpfiles.pp
@@ -4,17 +4,32 @@
 # @param noop Whether to run in noop. Defaults to false (ie: will remove files)
 plan profile::windows_tidy_c_tmpfiles (
   TargetSpec $targets,
+  Stdlib::Absolutepath $tidypath = 'C:\\',
   Boolean $recurse = false,
   Boolean $noop = false,
 ) {
+  run_plan(facts, targets => $targets, '_catch_errors' => true)
+
+  # Filter windows targets only
+  $windows_targets = get_targets($targets).filter | $target | {
+    $target.facts['os']['name'] == 'windows'
+  }
+
   # https://puppet.com/docs/puppet/6.18/types/tidy.html
   # to use matches, recurse must be non-zero/non-false.
   # specify recurse = 1 to not descend into subdirectories
-  $results = apply($targets, '_noop' => $noop) {
-    notify { get_target($targets).name: }
-    #class { 'profile::windows_tidy_c_tmpfiles':
-    #  recurse => $recurse,
-    #}
+  if ! $recurse {
+    $recurse_real = 1
+  } else {
+    $recurse_real = $recurse
+  }
+
+  $results = apply($windows_targets, '_catch_errors' => true, '_noop' => $noop) {
+    tidy { 'c_tmpfiles':
+      path    => $tidypath,
+      matches => [ '*.tmp' ],
+      recurse => $recurse_real,
+    }
   }
 
   return($results)

d9c677ebd727529a4e1eeb742302e2ccb3cd018e
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-11 15:15:34 +1000

    add params

--- site-modules/profile/plans/windows_tidy_c_tmpfiles.pp
+++ site-modules/profile/plans/windows_tidy_c_tmpfiles.pp
@@ -1,6 +1,11 @@
 # plan to delete *.tmp files under C:\
+# @param targets Targets to tidy
+# @param recurse Whether to recursively descend to tidy. Defaults to false.
+# @param noop Whether to run in noop. Defaults to false (ie: will remove files)
 plan profile::windows_tidy_c_tmpfiles (
   TargetSpec $targets,
+  Boolean recurse = false,
+  Boolean noop = false,
 ){
   $results = apply($targets, '_catch_errors' => true, '_noop' => true) {
     # https://puppet.com/docs/puppet/6.18/types/tidy.html
@@ -8,7 +13,8 @@ plan profile::windows_tidy_c_tmpfiles (
       path    => 'C:\\',
       age     => 0,
       matches => [ '*.tmp' ],
-      recurse => false,
+      recurse => $recurse,
+      noop    => $noop,
     }
   }
 }

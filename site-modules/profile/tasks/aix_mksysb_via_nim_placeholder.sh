ff31433e2a8edebde00fa08f2d165f6ef277c9bb
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-11 18:41:44 +1000

    aix mods

--- site-modules/profile/tasks/aix_mksysb_via_nim_placeholder.sh
+++ site-modules/profile/tasks/aix_mksysb_via_nim_placeholder.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 # Placeholder: run on the NIM server to create mksysb for NIM client
 NIMCLIENT=${PT_nimclient:?"ERROR: NIM client is required"}
+LOCATION=${PT_location:?"ERROR: location is required"}
 
 # any additional validation (eg: only run on AIX, confirm this is being run from NIM server)
 
 echo "Placeholder: run on NIM server $(uname -n) to create mksysb for ${NIMCLIENT}"
+
+# Example command
+# nim -o define -t mksysb -a mk_image=yes -a source=hawk -a location=/export/nim/mksysb/hawkmksysb -a server=master hawkmksysb
+
+PLACEHOLDER_CMD="nim -o define -t mksysb -a mk_image=yes -a source=${NIMCLIENT} -a location=${LOCATION}/${NIMCLIENT}mksysb -a server=master ${NIMCLIENT}mksysb"
+
+echo "Placeholder: mksysb example run from $(uname -n): ${PLACEHOLDER_CMD}"

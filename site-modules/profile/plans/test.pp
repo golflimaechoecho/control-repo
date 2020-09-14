276309ad1a37e7169ef3b401408c3f953de73c21
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-14 13:41:54 +1000

    vars

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -1,13 +1,14 @@
 plan profile::test (
   TargetSpec $targets,
 ) {
-  run_task(facter_task, $targets, '_catch_errors' => true)
+  #run_task(facter_task, $targets, '_catch_errors' => true)
   get_targets($targets).each | $target | {
-    $targetvars = $target.vars
-    run_command("echo vars for ${target}: ${targetvars}", $target)
+    $target_old_uri = $target.uri
+    run_command("echo 'uri for ${target}: ${target_old_uri}'", $target)
     $uri_upcase = upcase($target.uri)
     $target.set_var('uri', $uri_upcase)
-    run_command("echo 'updated vars for ${target}: ${targetvars}'", $target)
+    $target_new_uri = $target.uri
+    run_command("echo 'updated vars for ${target}: ${target_new_uri}'", $target)
   }
-  run_plan(facts, targets => $targets)
+  #run_plan(facts, targets => $targets)
 }

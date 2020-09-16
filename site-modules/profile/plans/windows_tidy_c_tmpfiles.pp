30366b47b4d893b96a7390e982cdd5b1bbfe3c81
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-16 20:07:25 +1000

    use run_command instead as tidy can't recurse on c:\

--- site-modules/profile/plans/windows_tidy_c_tmpfiles.pp
+++ site-modules/profile/plans/windows_tidy_c_tmpfiles.pp
@@ -1,4 +1,8 @@
-# plan to delete *.tmp files under C:\
+# @summary plan to delete *.tmp files under C:\
+#
+# originally planned to use tidy resource, however this fails with permission
+# denied errors for C:\Documents and Settings\...
+#
 # @param targets Targets to tidy
 # @param recurse Whether to recursively descend to tidy. Defaults to false.
 # @param noop Whether to run in noop. Defaults to false (ie: will remove files)
@@ -15,22 +19,11 @@ plan profile::windows_tidy_c_tmpfiles (
     $target.facts['os']['name'] == 'windows'
   }
 
-  # https://puppet.com/docs/puppet/6.18/types/tidy.html
-  # to use matches, recurse must be non-zero/non-false.
-  # specify recurse = 1 to not descend into subdirectories
-  if ! $recurse {
-    $recurse_real = 1
-  } else {
-    $recurse_real = $recurse
-  }
+  # extra backslash escaping
+  $command = "del ${tidypath}\\*.tmp /s /q"
+  out::message("Run tmpfile cleanup: ${command}")
 
-  $results = apply($windows_targets, '_catch_errors' => true, '_noop' => $noop) {
-    tidy { 'c_tmpfiles':
-      path    => $tidypath,
-      matches => [ '*.tmp' ],
-      recurse => $recurse_real,
-    }
-  }
+  $results = run_command($command, $windows_targets, '_catch_errors' => true, '_noop' => $noop)
 
   return($results)
 }

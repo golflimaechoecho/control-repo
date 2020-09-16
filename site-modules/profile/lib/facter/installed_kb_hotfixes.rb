f95d9138dea875f765a63043b60724abd4589f27
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-16 15:17:21 +1000

    capture3 returns string

--- site-modules/profile/lib/facter/installed_kb_hotfixes.rb
+++ site-modules/profile/lib/facter/installed_kb_hotfixes.rb
@@ -1,4 +1,5 @@
 # list installed hotfixes (KBs) for Windows only
+require 'json'
 require 'open3'
 
 Facter.add('installed_kb_hotfixes') do
@@ -7,14 +8,20 @@ Facter.add('installed_kb_hotfixes') do
   end
   setcode do
     installed_kb_hash = {}
-    # below outputs an array; convert to a hash with the hotfixid as keys
+
     stdout, stderr, status = Open3.capture3("Get-HotFix | Select-Object HotFixID, Description, InstalledBy, InstalledOn | ConvertTo-Json")
-    stdout.each do | eachkb |
+
+    # capture3 returns stdout as a string (representing an array of json hashes)
+    # parse this string into json; convert to a single hash with hotfixid as keys
+    hotfix_json = JSON.parse(stdout)
+
+    hotfix_json.each do | eachkb |
       # get the HotFixID to use as key
       hotfixid = eachkb['HotFixID']
       # add hotfixid as key, set value to the hash (minus the HotFixID key)
-      installed_kb_hash[hotfixid] = eachkb.delete('HotFixID')
+      installed_kb_hash[hotfixid] = eachkb.delete_if { | key, value | key == 'HotFixID' }
     end
+
     installed_kb_hash
   end
 end

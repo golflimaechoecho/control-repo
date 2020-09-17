90e71f7c800b968d2041fade16a83061705d8881
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-17 15:35:49 +1000

    print result report

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -7,10 +7,14 @@ plan profile::test (
   Optional[String[1]] $parameter = undef,
 ) {
   #run_task(facter_task, $targets, '_catch_errors' => true)
-  apply($targets) {
+  #run_plan(facts, targets => $targets)
+  $results = apply($targets) {
     $_parameter = lookup('profile::test::parameter', { 'default_value' => undef })
     $vsphere_host = lookup('profile::test::vsphere_host')
     notify { "echo parameter is ${parameter}, _parameter is ${_parameter}, vsphere host is ${vsphere_host}": }
   }
-  #run_plan(facts, targets => $targets)
+  $results.each | $result | {
+    out::message($result.report)
+  }
+  return()
 }

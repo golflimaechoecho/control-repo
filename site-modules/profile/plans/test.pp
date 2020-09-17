be3aaf46258a61206bc15cafe4d6cdeabb5b2d23
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-17 14:46:38 +1000

    missing brace

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -3,7 +3,7 @@ plan profile::test (
   Optional[String[1]] $parameter = lookup('profile::test::parameter', { 'default_value' => undef }),
 ) {
   #run_task(facter_task, $targets, '_catch_errors' => true)
-  apply($targets)
+  apply($targets) {
     $vsphere_host = lookup('profile::test::vsphere_host')
     notify { "echo parameter is ${parameter}, vsphere host is ${vsphere_host}": }
   }

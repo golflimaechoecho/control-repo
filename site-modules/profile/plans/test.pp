2ac5a2dcfb4e16799198295871fa45439653a8a4
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-17 15:09:35 +1000

    PE doesn't appear to support plan_hierarchy yet :sadface:

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -1,11 +1,16 @@
+# @summary test plan
+#
+# bolt supports --hiera-config and using plan_hierarchy, this doesn't appear available in PE yet
+#
 plan profile::test (
   TargetSpec $targets,
-  Optional[String[1]] $parameter = lookup('profile::test::parameter', { 'default_value' => undef }),
+  Optional[String[1]] $parameter = undef,
 ) {
   #run_task(facter_task, $targets, '_catch_errors' => true)
   apply($targets) {
+    $_parameter = lookup('profile::test::parameter', { 'default_value' => undef })
     $vsphere_host = lookup('profile::test::vsphere_host')
-    notify { "echo parameter is ${parameter}, vsphere host is ${vsphere_host}": }
+    notify { "echo parameter is ${parameter}, _parameter is ${_parameter}, vsphere host is ${vsphere_host}": }
   }
   #run_plan(facts, targets => $targets)
 }

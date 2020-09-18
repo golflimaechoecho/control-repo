06b843c8c44fd5d37a921934263a475e9374068a
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-18 13:43:12 +1000

    json

--- site-modules/profile/tasks/administrators_group_members.rb
+++ site-modules/profile/tasks/administrators_group_members.rb
@@ -12,6 +12,7 @@ if (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
   puppet_cmd = 'puppet'
   members_stdout, stderr, status = Open3.capture3("#{puppet_cmd} resource group Administrators --to_yaml")
   members_json = JSON.generate(YAML.load(members_stdout))
+  puts members_json
   members_json.each do | groupdef |
     puts groupdef['Administrators']['members']
   end

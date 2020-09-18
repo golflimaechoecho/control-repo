30784611b6e7d6b23e1c4fea7e59e4f07a1a1199
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2020-09-18 13:49:22 +1000

    parse generated output

--- site-modules/profile/tasks/administrators_group_members.rb
+++ site-modules/profile/tasks/administrators_group_members.rb
@@ -11,10 +11,8 @@ if (RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/)
   #puppet_cmd = "#{ENV['programfiles']}/Puppet Labs/Puppet/bin/puppet"
   puppet_cmd = 'puppet'
   members_stdout, stderr, status = Open3.capture3("#{puppet_cmd} resource group Administrators --to_yaml")
-  members_json = JSON.generate(YAML.load(members_stdout))
-  puts members_json
-  members_json.each do | groupdef |
-    puts groupdef['Administrators']['members']
+  members_json = JSON.parse(JSON.generate(YAML.load(members_stdout)))
+  puts members_json['group']['Administrators']['members']
   end
 else
   puts 'This task is only supported on Windows'

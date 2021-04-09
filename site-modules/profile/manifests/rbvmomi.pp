849f6c429ade40423cd10352d82704c20901c41e
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-09 15:30:25 +1000

    add make rpm dependency

--- site-modules/profile/manifests/rbvmomi.pp
+++ site-modules/profile/manifests/rbvmomi.pp
@@ -4,7 +4,7 @@ class profile::rbvmomi {
   $gem_pkgs = [ 'builder', 'json', 'nokogiri', 'mini_portile', 'rbvmomi' ]
 
   # package dependencies for nokogiri java native extension
-  $dep_rpms = [ 'gcc', 'rpm-build', 'ruby-devel', 'zlib-devel' ]
+  $dep_rpms = [ 'make', 'gcc', 'rpm-build', 'ruby-devel', 'zlib-devel' ]
 
   package { $dep_rpms:
     ensure => installed,

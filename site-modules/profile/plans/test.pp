e4ab771bee48bb9b0074a898f788bb44295fc083
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-15 09:12:44 +1000

    test plan

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -11,9 +11,28 @@ plan profile::test (
   apply_prep($targets)
   $parameter = lookup('profile::test::parameter', { 'default_value' => undef })
   $results = apply($targets) {
-    $_parameter = lookup('profile::test::parameter', { 'default_value' => undef })
-    $vsphere_host = lookup('profile::test::vsphere_host')
-    notify { "echo parameter is ${parameter}, _parameter is ${_parameter}, vsphere host is ${vsphere_host}": }
+    #$_parameter = lookup('profile::test::parameter', { 'default_value' => undef })
+    #$vsphere_host = lookup('profile::test::vsphere_host')
+    #notify { "echo parameter is ${parameter}, _parameter is ${_parameter}, vsphere host is ${vsphere_host}": }
+    $vsphere_servers = lookup('profile::pe_patch::vsphere_servers')
+    $vsphere_host = lookup('profile::pe_patch::vsphere_host')
+    notify { "servers ${vsphere_servers}, host ${vsphere_host}": }
+    #if $vsphere_host in $vsphere_servers {
+    #  $targets.each | $node | {
+    #    notify { [ $node,
+    #               'pe_patch_snapshot',
+    #               $vsphere_host,
+    #               $vsphere_servers[$vsphere_host][$vsphere_username],
+    #               $vsphere_servers[$vsphere_host][$vsphere_password],
+    #               $vsphere_servers[$vsphere_host][$vsphere_datacenter],
+    #               $vsphere_servers[$vsphere_host][$vsphere_insecure],
+    #               '',
+    #               false,
+    #               true,
+    #               'create' ]:
+    #    }
+    #  }
+    #}
   }
   $results.each | $result | {
     $report = $result.report['resource_statuses']

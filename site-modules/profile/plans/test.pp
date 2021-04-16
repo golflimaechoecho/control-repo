d06fbd5529fedd54fe5f25e2c346400b1354fa31
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-16 12:31:40 +1000

    nope, hiera lookups get mixed up if apply not in target loop?

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -11,13 +11,11 @@ plan profile::test (
   # patching::snapshot_vmware appears to take vsphere details from first target (ie: assumes all on same)
   # to be able to lookup indiv details, need to iterate on loop ie: take snapshots serially not in parallel
   # Work out vm_names to snapshot (hardcoded to hostname here)
-  #$vm_names = patching::target_names($targets, 'hostname')
   $_targets = $targets.get_targets()
-  $snap_results = apply($_targets) {
-    $_targets.each | $target | {
+  $_targets.each | $target | {
+    $snap_result = apply($target) {
       $snapname = regsubst($target.uri, '^([^.]+).*','\1')
       notify { $snapname: }
-      #$snap_result = apply($target) {
       $vsphere_servers = lookup('profile::pe_patch::vsphere_servers')
       $vsphere_host = lookup('profile::pe_patch::vsphere_host')
       $vsphere_username = $vsphere_servers[$vsphere_host]['vsphere_username']
@@ -26,11 +24,11 @@ plan profile::test (
         notify { "${snapname}, host: ${vsphere_host}, other: ${vsphere_username}, ${vsphere_servers[$vsphere_host]['vsphere_password']}, ${vsphere_servers[$vsphere_host]['vsphere_datacenter']}, ${vsphere_servers[$vsphere_host]['vsphere_insecure']}": }
       }
     }
-    #$report = $snap_result.results[0].report['resource_statuses']
-    #out::message("report is $report")
-  }
-  $snap_results.each | $result | {
-    $report = $result.report['resource_statuses']
+    $report = $snap_result.results[0].report['resource_statuses']
     out::message("report is $report")
   }
+  #$snap_results.each | $result | {
+  #  $report = $result.report['resource_statuses']
+  #  out::message("report is $report")
+  #}
 }

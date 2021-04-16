53418e776dead8613ff74bdeb861ec651c4df2d0
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-16 10:09:32 +1000

    more debug

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -4,7 +4,6 @@
 #
 plan profile::test (
   TargetSpec $targets,
-  #Optional[String[1]] $parameter = undef,
 ) {
   #run_task(facter_task, $targets, '_catch_errors' => true)
   #run_plan(facts, targets => $targets)
@@ -16,23 +15,12 @@ plan profile::test (
     #notify { "echo parameter is ${parameter}, _parameter is ${_parameter}, vsphere host is ${vsphere_host}": }
     $vsphere_servers = lookup('profile::pe_patch::vsphere_servers')
     $vsphere_host = lookup('profile::pe_patch::vsphere_host')
-    notify { "servers ${vsphere_servers}, host ${vsphere_host}": }
-    #if $vsphere_host in $vsphere_servers {
-    #  $targets.each | $node | {
-    #    notify { [ $node,
-    #               'pe_patch_snapshot',
-    #               $vsphere_host,
-    #               $vsphere_servers[$vsphere_host][$vsphere_username],
-    #               $vsphere_servers[$vsphere_host][$vsphere_password],
-    #               $vsphere_servers[$vsphere_host][$vsphere_datacenter],
-    #               $vsphere_servers[$vsphere_host][$vsphere_insecure],
-    #               '',
-    #               false,
-    #               true,
-    #               'create' ]:
-    #    }
-    #  }
-    #}
+    #{ "servers ${vsphere_servers}, host ${vsphere_host}": }
+
+    $vm_names = patching::target_names($targets, 'hostname')
+    if $vsphere_host in $vsphere_servers {
+      notify { "vm_names: ${vm_names},  host: ${vsphere_host}, other: ${vsphere_servers[$vsphere_host][$vsphere_username]}, ${vsphere_servers[$vsphere_host][$vsphere_password]}, ${vsphere_servers[$vsphere_host][$vsphere_datacenter]}, ${vsphere_servers[$vsphere_host][$vsphere_insecure]}" }
+    }
   }
   $results.each | $result | {
     $report = $result.report['resource_statuses']

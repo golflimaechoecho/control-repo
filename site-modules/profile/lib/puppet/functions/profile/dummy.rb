2c44dde8f041002b072e96f931813020e446eb8b
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-16 18:35:45 +1000

    to_json

--- site-modules/profile/lib/puppet/functions/profile/dummy.rb
+++ site-modules/profile/lib/puppet/functions/profile/dummy.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'net/http'
+require 'json'
 
 # Nokogiri stupidness
 Puppet::Functions.create_function(:'profile::dummy') do
@@ -24,8 +25,9 @@ Puppet::Functions.create_function(:'profile::dummy') do
     headers = { 'content-type' => 'application/json', 'X-Authentication' => '0RxZgd2x2QOr9OglKGuKTQYl_ErVLs5NtMNYyDT-3ZtQ' }
     @http = Net::HTTP.new(host, port)
 
-    @http.request_post('/command/task', data, headers)
+    @http.request_post('/command/task', data.to_json, headers)
     nk = Nokogiri(response.body)
+    # this is expecting xml rather than whatever orchestrator is going to return but let's roll with it for now
     [nk.xpath('//soapenv:Body/*').select(&:element?).first, response.body.size]
   end
 end

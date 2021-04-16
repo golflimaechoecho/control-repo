51d43f800fe42cf39e1d439b6350624ceae6e9b9
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-16 11:54:28 +1000

    no curly braces

--- site-modules/profile/plans/test.pp
+++ site-modules/profile/plans/test.pp
@@ -19,7 +19,7 @@ plan profile::test (
       $vsphere_servers = lookup('profile::pe_patch::vsphere_servers')
       $vsphere_host = lookup('profile::pe_patch::vsphere_host')
       $vsphere_username = $vsphere_servers[$vsphere_host]['vsphere_username']
-      $snapname = ${target}.host
+      $snapname = $target.host
 
       if $vsphere_host in $vsphere_servers {
         notify { "${snapname}, host: ${vsphere_host}, other: ${vsphere_username}, ${vsphere_servers[$vsphere_host]['vsphere_password']}, ${vsphere_servers[$vsphere_host]['vsphere_datacenter']}, ${vsphere_servers[$vsphere_host]['vsphere_insecure']}": }

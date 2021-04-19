23af105ba449c69c0bc5267c9cfe688a36e28623
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-19 12:17:11 +1000

    setup vsphere ext fact based on hiera

---
+++ site-modules/profile/manifests/vsphere_details.pp
@@ -0,0 +1,28 @@
+# @summary write out vsphere details to external fact file
+# this is somewhat backwards as looking up details from hiera to set the fact
+class profile::vsphere_details {
+  $vsphere_servers = lookup('profile::pe_patch::vsphere_servers')
+  $vsphere_host = lookup('profile::pe_patch::vsphere_host')
+
+  if $vsphere_host in $vsphere_servers {
+   # external fact directories: https://puppet.com/docs/puppet/latest/external_facts.html
+    case $facts['os']['family'] {
+      'windows': {
+        $extfactdir = 'C:/ProgramData/PuppetLabs/facter/facts.d'
+      }
+      default: {
+        $extfactdir = '/etc/puppetlabs/facter/facts.d'
+      }
+    }
+
+    file { "${extfactdir}/vsphere_details.yaml":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => epp("${module_name}/vsphere_details.epp", { 'vsphere_host' => $vsphere_host, 'vsphere_details' => $vsphere_servers[$vsphere_host] }),
+    }
+  } else {
+    fail("Unable to find specified vsphere_host ${vsphere_host} for ${trusted['certname']}")
+  }
+}

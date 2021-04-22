18f1283b6572af40a7c201c6f461c3531d256ec2
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-04-22 16:03:21 +1000

    notify

--- site-modules/profile/manifests/test.pp
+++ site-modules/profile/manifests/test.pp
@@ -1,4 +1,14 @@
 # test.pp
 class profile::test {
-  include potato::nobueno
+  if defined("potato::nobueno") {
+    notify { "potato defined": }
+  } else {
+    notify { "potato not defined": }
+  }
+  if defined("pe_nc_backup") {
+    notify { "pe_nc_backup defined": }
+  } else {
+    notify { "pe_nc_backup not defined": }
+  }
+  #include potato::nobueno
 }

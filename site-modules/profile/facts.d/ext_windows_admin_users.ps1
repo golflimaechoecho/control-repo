1e4a31bc20ae2945cb1b6a6052e327a4895fadc8
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-05-20 12:01:31 +1000

    missing $

--- site-modules/profile/facts.d/ext_windows_admin_users.ps1
+++ site-modules/profile/facts.d/ext_windows_admin_users.ps1
@@ -1,4 +1,4 @@
 #Requires -Version 3.0
 $query = {$Ms = net localgroup administrators | where {$_ -AND $_ -notmatch 'command completed successfully'} | select -skip 4;foreach($m in $Ms){$users += $M+' , '}; return $Users}
-$result = & query
+$result = & $query
 Write-Output "{""ext_windows_admin_users"": ${result} }"

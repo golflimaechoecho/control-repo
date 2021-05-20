b23309fabad2d173342e801088d8e9b80da65d0f
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-05-20 11:26:59 +1000

    external fact typos

--- site-modules/profile/facts.d/ext_windows_admin_users.ps1
+++ site-modules/profile/facts.d/ext_windows_admin_users.ps1
@@ -1,3 +1,3 @@
 #Requires -Version 3.0
-$result = "$Ms = net localgroup administrators | where {$_ -AND $_ -notmatch 'command completed successfully'} | select -skip 4;foreach($m in $Ms){$users += $M+' , '}; return $Users"
+$result = ($Ms = net localgroup administrators | where {$_ -AND $_ -notmatch 'command completed successfully'} | select -skip 4;foreach($m in $Ms){$users += $M+' , '}; return $Users)
 Write-Output "{""ext_windows_admin_users"": ${result} }"

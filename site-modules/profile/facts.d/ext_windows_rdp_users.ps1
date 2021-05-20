0dd94b86e70e06c5af7929ad4ee157f1808793e8
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-05-20 11:58:59 +1000

    use script block instead

--- site-modules/profile/facts.d/ext_windows_rdp_users.ps1
+++ site-modules/profile/facts.d/ext_windows_rdp_users.ps1
@@ -1,4 +1,4 @@
 #Requires -Version 3.0
-$Ms = (net localgroup 'Remote Desktop Users' | where {$_ -AND $_ -notmatch 'command completed successfully'} | select -skip 4)
-$result = foreach($m in $Ms){$users += $M+' , '}; return $Users
+$query = {$Ms = net localgroup 'Remote Desktop Users' | where {$_ -AND $_ -notmatch 'command completed successfully'} | select -skip 4 ;foreach($m in $Ms){$users += $M+' , '}; return $Users}
+$result = & $query
 Write-Output "{""ext_windows_rdp_users"": ${result} }"

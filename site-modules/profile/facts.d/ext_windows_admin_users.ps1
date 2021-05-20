edb076f922bd7b720560802413a2f8e83ba9613f
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-05-20 11:51:07 +1000

    external ps1 fact formatting

--- site-modules/profile/facts.d/ext_windows_admin_users.ps1
+++ site-modules/profile/facts.d/ext_windows_admin_users.ps1
@@ -1,3 +1,4 @@
 #Requires -Version 3.0
-$result = ($Ms = net localgroup administrators | where {$_ -AND $_ -notmatch 'command completed successfully'} | select -skip 4;foreach($m in $Ms){$users += $M+' , '}; return $Users)
+$Ms = (net localgroup administrators | where {$_ -AND $_ -notmatch 'command completed successfully'} | select -skip 4)
+$result = foreach($m in $Ms){$users += $M+' , '}; return $Users
 Write-Output "{""ext_windows_admin_users"": ${result} }"

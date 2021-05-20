dfb10ed20d74c4a68c5f72d2bcc17eb80a0fbdb8
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2021-05-20 13:48:21 +1000

    catch

--- site-modules/profile/facts.d/ext_winupdateserver.ps1
+++ site-modules/profile/facts.d/ext_winupdateserver.ps1
@@ -1,5 +1,5 @@
 #Requires -Version 3.0
-function GetWUServer {
+function Get-WinUpdateServer {
   # powershell 5+ could use Get-ItemPropertyValue, left as Get-ItemProperty for older versions
   try {
     (Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate").WUServer
@@ -7,9 +7,6 @@ function GetWUServer {
   catch {
     (Get-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\WindowsUpdate").WUServer
   }
-  catch {
-    "Unable to find WUServer"
-  }
 }
-$result = GetWUServer
+$result = GetWinUpdateServer
 Write-Output "{""ext_winupdateserver"": ${result} }"
